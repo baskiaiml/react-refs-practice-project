c401f7fa552784c6ae5c8d47ed144ba42530b392
skillprofile-user-at-417125717649 <baski.ciet@gmail.com>
2025-04-11 08:17:46 +0530

    Added Changes to the form and Progress side bar

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,12 @@
-export default function Input({label, isTextArea, ...props}){
+import { forwardRef } from "react";
+const Input = forwardRef(function Input({label, isTextArea, ...props}, ref){
 
     const classesInput = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
     return (
         <p className="flex flex-col gap-1 my-4">
         <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-        {isTextArea?   <textarea className={classesInput} {...props}/>:<input className={classesInput} {...props}/>}
+        {isTextArea?   <textarea ref={ref} className={classesInput} {...props}/>:<input ref={ref} className={classesInput} {...props}/>}
     </p>
     )
-}
+});
+export default Input;
